dee9c7bb4698ff114a9339657a2224929e1beecb
slown137 <s.lown137@gmail.com>
2021-06-01 18:12:27 -0400

    generateMarkdown written

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,13 +10,48 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
+
+function gitLink(username) {
+  return `https://github.com/${username}`
+};
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-/*   return `# ${data.title}
+  return `# ${data.title}
 
-`; */
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  ${data.desc}
 
+  ## Table of Content
+  [Installation](#Installation)
+  [Usage](#Usage)
+  [License](#License)
+  [Contributing](#Contributing)
+  [Tests](#Tests)
+  [Questions](#Questions)
 
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${renderLicenseLink(data.license)}
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contribute}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  GitHub: ${data.githubUser}
+  ${gitLink(data.githubUser)}
+`;
 }
 
 module.exports = generateMarkdown;
