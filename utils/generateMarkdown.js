75bc779189fd398c30871244c28537e0f7b73bb2
slown137 <s.lown137@gmail.com>
2021-06-01 18:56:34 -0400

    added links to badge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,18 +4,17 @@ function renderLicenseBadge(data) {
   if (data.license === 'Apache License 2.0') {
     return "<a href=https://www.apache.org/licenses/LICENSE-2.0><img src=\"https://img.shields.io/badge/License-Apache%202.0-green\">"
   } else if (data.license === "MIT License") {
-    return "<a href=https://www.apache.org/licenses/LICENSE-2.0><img src=\"https://img.shields.io/badge/License-MIT%202.0-green\">"
+    return "<a href=https://www.mit.edu/~amini/LICENSE.md><img src=\"https://img.shields.io/badge/License-MIT%202.0-green\">"
   } else if (data.license === "GNU General Public License v2.0") {
-    return "<a href=https://www.apache.org/licenses/LICENSE-2.0><img src=\"https://img.shields.io/badge/License-GNUGPLv2%202.0-green\">"
+    return "<a href=https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html><img src=\"https://img.shields.io/badge/License-GNUGPLv2%202.0-green\">"
   } else if (data.license === "GNU General Public License v3.0") {
-    return "<a href=https://www.apache.org/licenses/LICENSE-2.0><img src=\"https://img.shields.io/badge/License-GNUGPLv3%202.0-green\">"
+    return "<a href=https://www.gnu.org/licenses/gpl-3.0.en.html><img src=\"https://img.shields.io/badge/License-GNUGPLv3%202.0-green\">"
   } else if (data.license === "BSD 2-clause \"Simplified\" license") {
-    return "<a href=https://www.apache.org/licenses/LICENSE-2.0><img src=\"https://img.shields.io/badge/License-BSD2Clause%202.0-green\">"
+    return "<a href=https://opensource.org/licenses/BSD-2-Clause><img src=\"https://img.shields.io/badge/License-BSD2Clause%202.0-green\">"
   } else if (data.license === "BSD 3-clause \"New\" or \"Revised\" license") {
-    return "<a href=https://www.apache.org/licenses/LICENSE-2.0><img src=\"https://img.shields.io/badge/License-BSD3Clause%202.0-green\">"
-  }
-
-}
+    return "<a href=https://opensource.org/licenses/BSD-3-Clause><img src=\"https://img.shields.io/badge/License-BSD3Clause%202.0-green\">"
+  };
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
